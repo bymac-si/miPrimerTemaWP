71e33a046471539b19b7ee3837a6d84acafeb5e5
Marcos Castro Abarca <61596615+bymac-si@users.noreply.github.com>
2022-08-10 12:34:39 -0400

    first commit

---
+++ functions.php
@@ -0,0 +1,61 @@
+<?php
+
+// Custom Logo
+add_theme_support( 'custom-logo', array(
+	'height'      => 100,
+	'width'       => 300,
+	'flex-height' => true,
+	'flex-width'  => true,
+	'header-text' => array( 'site-title', 'site-description' ),
+) );
+
+// Theme Files
+function theme_files()
+{
+	wp_enqueue_style( 'main-style',get_template_directory_uri() . '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'theme_files' );
+
+// Title Tag Support
+add_theme_support( 'title-tag' );
+
+
+// Add HTML5 theme support.
+
+function wpdocs_after_setup_theme() {
+    add_theme_support( 'html5', array( 'search-form' ) );
+}
+add_action( 'after_setup_theme', 'wpdocs_after_setup_theme' );
+
+// Custom Widgets
+function custom_widgets() {
+
+	register_sidebar( array(
+		'name'          => 'Home Banner Slider',
+		'id'            => 'home_banner_slider',
+		'before_widget' => '',
+		'after_widget'  => '',
+		'before_title'  => '',
+		'after_title'   => '',
+	) );
+
+	register_sidebar( array(
+		'name'          => 'Home Sidebar',
+		'id'            => 'home_sidebar',
+		'before_widget' => '<aside class="home_side_widget">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h3>',
+		'after_title'   => '</h3>',
+	) );
+
+	register_sidebar( array(
+		'name'          => 'Home Footer Widgets',
+		'id'            => 'home_footer_widgets',
+		'before_widget' => '<aside class="footer-widgets">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h4>',
+		'after_title'   => '</h4>',
+	) );
+
+}
+add_action( 'widgets_init', 'custom_widgets' );
---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+        <link rel="profile" href="http://gmpg.org/xfn/11" />
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+        <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
+        <?php wp_head(); ?>
+</head>
+<body>
+    <div id="container">
+<header class="main-header">
+    <div class="custom-logo">
+      <a href="<?php echo bloginfo( 'url' );?>">  
+        <?php
+
+        $custom_logo_id = get_theme_mod( 'custom_logo' );
+        $custom_logo_url = wp_get_attachment_image_url( $custom_logo_id , 'full' );
+        echo '<img src="' . esc_url( $custom_logo_url ) . '" alt="">';
+        
+        ?>
+        </a>
+    </div>
+    <div class="search">
+        <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+            
+        <input type="search" class="search-field"
+            placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>"
+            value="<?php echo get_search_query() ?>" name="s"
+            title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
+ 
+    <input type="submit" class="search-submit"
+        value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
+</form>
+    </div>
+    <nav class="main-menu">
+         <?php wp_nav_menu(); ?>
+    </nav>
+</header>
+
+<section class="banner">
+<?php if ( is_active_sidebar( 'home_banner_slider' ) ) : ?>
+	
+		<?php dynamic_sidebar( 'home_banner_slider' ); ?>
+	
+<?php endif; ?>
+</section>
+<section class="content-holder">
+    <article class="content">
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <div class="article">
+        <h2><a href="<?php the_permalink() ?>"><?php echo get_the_title(); ?></a></h2>
+        <?php the_excerpt(); ?>
+    </div>
+    <?php endwhile; else : ?>
+	<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
+    </article>
+    <aside class="sidebar">
+    <?php if ( is_active_sidebar( 'home_sidebar' ) ) : ?>
+    <?php dynamic_sidebar( 'home_sidebar' ); ?>
+<?php endif; ?>
+    </aside>
+</section>
+<footer class="main-footer">
+<?php if ( is_active_sidebar( 'home_footer_widgets' ) ) : ?>
+    <?php dynamic_sidebar( 'home_footer_widgets' ); ?>
+<?php endif; ?>
+</footer>
+<div class="powered">
+    <p><small>Powered by <a href="<?php echo bloginfo( 'url' ); ?>"><?php echo bloginfo( 'name' ); ?></a> | <?php echo date ('Y'); ?></small></p>
+</div>
+    </div>
+</body>
+</html>
